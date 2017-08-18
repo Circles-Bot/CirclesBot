7ca5d35838af064f31dd3f1a8a49463445ffc167
InsertNullHere <31144424+InsertNullHere@users.noreply.github.com>
2017-08-18 18:08:49 -0400

    Bonjour
    Changed very minor things. Love you long time
    
    ~Null

--- bot.js
+++ bot.js
@@ -1,15 +1,14 @@
 // Required
-const Discord = require("discord.js");
-
-const Token = "MzQ4MjE1NzEwMzQ4MjE0Mjc0.DHj1JA.2-7VYWyIDCXA7fM4rRaLEBKo8PI";
-const Prefix = ".";
+const discord = require("discord.js");
+const token = "MzQ4MjE1NzEwMzQ4MjE0Mjc0.DHj1JA.2-7VYWyIDCXA7fM4rRaLEBKo8PI";
+const prefix = ".";
 
 var bot = new Discord.Client();
 
 // Bot Ready
 bot.on("ready", function() {
-	console.log("Logic Bot is ready!");
+	console.log("Circles Bot is ready!");
 });
 
 // Bot Login
-bot.login(Token);
+bot.login(token);
