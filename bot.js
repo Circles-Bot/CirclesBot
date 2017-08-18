b00a0aab5bc3103beaadcb4eed20ff26ae00073c
Cody <ProjectChaosYT@outlook.com>
2017-08-18 18:46:44 -0400

    changed bot.js again

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@
 const botSettings = require("./botsettings.json");
 const discord = require("discord.js");
 const prefix = botSettings.prefix;
+const bot = new discord.Client({disableEveryone: true});
 
 // Variables
-var bot = new discord.Client({disableEveryone: true});
 
 // Bot Ready
-bot.on("ready", function() {
+bot.on("ready", async () => {
 	console.log(`Bot is ready! ${bot.user.username}`);
 });
 
