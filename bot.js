ad3b144bb0dc14897d35914cdffc731ebc1e0450
InsertNullHere <31144424+InsertNullHere@users.noreply.github.com>
2017-08-18 18:20:23 -0400

    Penis
    Organized it more.
    
    8========D

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const botSettings = require("./botsettings.json");
 const discord = require("discord.js");
 const prefix = botSettings.prefix;
 const bot = new Discord.Client({disableEveryone: true});
+// Variables
 
 // Bot Ready
 bot.on("ready", async () => {
