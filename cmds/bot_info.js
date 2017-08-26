8846130f294878d3d98d483cf1727faa03774a0a
Cody <ProjectChaosYT@outlook.com>
2017-08-26 15:54:25 -0400

    Changed random shit idek

--- cmds/bot_info.js
+++ cmds/bot_info.js
@@ -4,7 +4,9 @@ module.exports.run = (bot, message, args) => {
     let embed = new discord.RichEmbed()
         .setTitle("About Circles Bot")
         .setThumbnail("http://i.imgur.com/8UuwIEb.png")
-        .setDescription("Circles Bot is a multipurpose Discord Bot built for your enjoyment and ease. \n \n[Official Website](https://circlesbot.mistforums.com) \n[Official Server](https://discord.gg/aDAdUgG) \n[Invite Circles](https://discordapp.com/oauth2/authorize?client_id=348215710348214274&scope=bot&permissions=535850047) \n[Donate](https://www.patreon.com/circlesbot)")
+        .setDescription("Circles Bot is a multipurpose Discord Bot built for your enjoyment and ease.")
+        .addField("Created By", "ProjectChaos#6395 & Null#3081")
+        .addField("Information", "[Official Website](https://circlesbot.mistforums.com) \n[Official Server](https://discord.gg/aDAdUgG) \n[Invite Circles](https://discordapp.com/oauth2/authorize?client_id=348215710348214274&scope=bot&permissions=535850047) \n[Donate](https://www.patreon.com/circlesbot)")
         .setColor("#4292f4")
 
     message.channel.send({embed: embed});
