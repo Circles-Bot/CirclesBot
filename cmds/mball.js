d7ba54696859520a975f552f0f438b201228910f
InsertNullHere <31144424+InsertNullHere@users.noreply.github.com>
2017-08-27 19:35:22 -0400

    maybe?

--- cmds/mball.js
+++ cmds/mball.js
@@ -7,7 +7,7 @@ var fortunes = [
 
 module.exports.run = (bot, message, args) => {
     if (args[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
-    else message.reply("Can't read that. Please use `.mball [Ask a question]`");
+    else message.reply("Ya gotta ask a question. Please use `.mball [Ask a question]`");
 };
 
 // Name is the command name, .mball will initialize the command
